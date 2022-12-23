337b949d807cbaf17512763846e728e12e309dee
Abhishek <hunter@localhost.localdomain>
2022-12-23 16:54:29 +0545

    form validation

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,9 +1,12 @@
+import { CssSyntaxError } from "postcss";
 import React, { useCallback, useEffect, useState } from "react";
 import JsonData from "../api/newForm.json";
+import { validataion } from "../validataion/formValidataion";
 
 const Form = () => {
   // const [itemList, setItemList] = useState([]);
   const [emptyObjState, setEmptyObjState] = useState({});
+  const [errors, setErrors] = useState({});
 
   // const emptyObj = { name: "", price: "", category: "", other: "" };
   const [data, setData] = useState({});
@@ -11,7 +14,7 @@ const Form = () => {
   const inputClassName =
     "bg-gray-50 border outline-none border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500 placeholder:capitalize";
   const lableClassName =
-    "block mb-2 text-sm font-bold text-gray-900 dark:text-white capitalize";
+    "mb-2 text-sm font-bold text-gray-900 dark:text-white capitalize";
 
   const getEmptyObj = useCallback(() => {
     return JsonData?.inputFiled.map(({ name }) => {
@@ -21,6 +24,7 @@ const Form = () => {
       return true;
     });
   }, []);
+  // console.log(">>data >>", data);
 
   useEffect(() => {
     getEmptyObj();
@@ -33,24 +37,45 @@ const Form = () => {
     return setData({ ...data, [name]: value });
   };
 
+  const onSubmitBtn = (e) => {
+    e.preventDefault();
+    const check = validataion(data);
+    if (Object.keys(check).length !== 0) setErrors(check);
+    console.log(check);
+  };
+
   // const generateID = () => {
   //   if (itemList.length === 0) return 1;
   //   const lastId = itemList?.slice(-1);
   //   return lastId[0]?.id + 1;
   // };
 
+  const ShowError = ({ name }) => {
+    if (errors.hasOwnProperty(name)) {
+      return <span className={"error_box"}>{errors[name]}</span>;
+    }
+    return false;
+  };
+
   return (
-    <section className="w-[50%]  mx-auto rounded-md my-5  ">
+    <section className="md:w-[70%] lg:w-[50%]  mx-auto rounded-md my-5  ">
       <div className="border px-8 py-6 rounded-md shadow-md">
         <h1 className="text-2xl font-bold text-center mb-8">Form !!!</h1>
-        <form className="flex flex-col justify-center  gap-4" method="post">
+        <form
+          className="flex flex-col justify-center  gap-4"
+          method="post"
+          onSubmit={onSubmitBtn}
+        >
           <div className="grid md:grid-cols-2 gap-x-8 gap-y-3">
             {JsonData?.inputFiled?.map(({ name, type }, indx) => {
               return (
                 <div key={indx} className="">
-                  <label htmlFor={name} className={lableClassName}>
-                    {name.replaceAll("_", " ")}
-                  </label>
+                  <div className="flex flex-row gap-2 items-center flex-wrap">
+                    <label htmlFor={name} className={lableClassName}>
+                      {name.replaceAll("_", " ")}
+                    </label>
+                    <ShowError name={name} />
+                  </div>
                   <input
                     type={type}
                     name={name.toLowerCase()}
@@ -67,7 +92,7 @@ const Form = () => {
 
           <div className="flex flex-row justify-end items-center gap-4">
             <button
-              type="button"
+              type="submit"
               className="capitalize text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
             >
               submit
---
+++ src/validataion/formValidataion.js
@@ -0,0 +1,107 @@
+import JsonData from "../api/newForm.json";
+
+export const validataion = (data) => {
+  let errors = {};
+  const { inputFiled } = JsonData;
+
+  inputFiled.forEach((item) => {
+    const { name, valid } = item;
+    const char = data[name];
+    const capitalizeName = name[0]
+      .toUpperCase()
+      .concat(name.slice(1))
+      .replaceAll("_", " ");
+
+    const newValid = Object.keys(valid);
+    for (let i = 0; i < newValid.length; i++) {
+      const key = newValid[i];
+      const value = valid[key];
+      // console.log(key, "=>", value);
+
+      switch (key) {
+        case "required":
+          if (char === "") {
+            return (errors = {
+              ...errors,
+              [name]: `${capitalizeName} is required`,
+            });
+          }
+          break;
+
+        case "min":
+          if (char.length < value) {
+            return (errors = {
+              ...errors,
+              [name]: `${capitalizeName} min ${value} character`,
+            });
+          }
+          break;
+
+        case "max":
+          if (key === "max" && char.length > value) {
+            return (errors = {
+              ...errors,
+              [name]: `${capitalizeName} max ${value} character`,
+            });
+          }
+          break;
+
+        case "type":
+          errors = {
+            ...errors,
+            ...charType({ name, char, value, capitalizeName }),
+          };
+          break;
+
+        case "match":
+          const newData = data[value];
+          errors = {
+            ...errors,
+            ...matchData({ name, char, capitalizeName, newData }),
+          };
+          break;
+
+        default:
+          return errors;
+      }
+    }
+
+    // break();
+  });
+
+  return errors;
+};
+
+const charType = ({ name, char, value, capitalizeName }) => {
+  const textOnly = /[^a-z]/gi;
+  const textAndNumberOnly = /[^a-z0-9]/gi;
+  const numberOnly = /[^0-9]/gi;
+  const emailOnly = /^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/;
+
+  switch (value) {
+    case "text":
+      if (!char.match(textOnly)) return;
+      return { [name]: `Do not use number` };
+
+    case "number":
+      if (!char.match(numberOnly)) return;
+      return { [name]: `Invalid ${capitalizeName}` };
+
+    case "textandnumber":
+      if (!char.match(textAndNumberOnly)) return;
+      return { [name]: `Invalid ${capitalizeName}` };
+
+    case "email":
+      if (char.match(emailOnly)) return;
+      return { [name]: `Invalid ${capitalizeName}` };
+
+    default:
+      return;
+  }
+};
+
+const matchData = ({ name, char,  capitalizeName, newData }) => {
+  console.log("match >> ", newData);
+  if (char === newData) return;
+  return { [name]: `${capitalizeName} does not match !!!` };
+};
